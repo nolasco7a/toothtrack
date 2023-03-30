41f846b3b0b677c064f60c3b4d278a7b9bc854cd
Allan Nolasco <allan.nolasco@paicbd.com>
2023-03-30 11:00:13 -0500

    add office dto

--- src/office/dto/create-office.dto.ts
+++ src/office/dto/create-office.dto.ts
@@ -1,1 +1,17 @@
-export class CreateOfficeDto {}
+import { IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Address } from '../entities/address.entity';
+export class CreateOfficeDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  address: Address;
+
+  @ApiProperty()
+  created_at: Date;
+
+  @ApiProperty()
+  created_by: string;
+}
--- src/office/dto/update-office.dto.ts
+++ src/office/dto/update-office.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOfficeDto } from './create-office.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+import { Address } from '../entities/address.entity';
+export class UpdateOfficeDto extends PartialType(CreateOfficeDto) {
+  @ApiProperty()
+  name: string;
 
-export class UpdateOfficeDto extends PartialType(CreateOfficeDto) {}
+  @ApiProperty()
+  address: Address;
+
+  @ApiProperty()
+  @IsString()
+  updated_at: Date;
+
+  @ApiProperty()
+  @IsString()
+  updated_by: string;
+}
