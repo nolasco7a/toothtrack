ec09e7e42aa4edb5f77df6d248ef7f69d764fec1
Allan Nolasco <allan.nolasco@paicbd.com>
2023-03-30 11:00:35 -0500

    add address entity

---
+++ src/office/entities/address.entity.ts
@@ -0,0 +1,10 @@
+export class Address {
+  street: string;
+  colony: string;
+  city: string;
+  state: string;
+  country: string;
+  zip_code: string;
+  phone_number: number;
+  status: string;
+}
